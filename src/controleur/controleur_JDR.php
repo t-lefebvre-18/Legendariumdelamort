f856b0276f1c66a62d383b9327c4e1aefd1e638e
rjousse18 <remi.jousse@free.fr>
2019-05-05 16:23:13 +0200

    - Système de demande de JDR par un Mj terminée. - Amélioration de 3-4 trucs graphique - Ajout de la table JDR dans la base de données (voir sur site si6)

--- src/controleur/controleur_JDR.php
+++ src/controleur/controleur_JDR.php
@@ -3,38 +3,60 @@
     {
         $form = array();
         $jeu = new Jeu($db);
-        if(isset($_GET['id']))
+        if(isset($_POST['btAjoutJDR']))
+        {
+            $pseudo = $_POST['inputPseudo'];
+            $email = $_POST['inputEmail'];
+            $tel = $_POST['inputTel'];
+            $synopsis = $_POST['inputSynopsis'];
+            if(isset($_POST['inputFournis']))
+                $fournis = true;
+            else
+                $fournis = false;
+            $nbrJoueur = $_POST['inputNbrJoueur'];
+            if(isset($_POST['dateDisp']))
+            {
+                $dates = $_POST['dateDisp'];
+                $listeDates = "";
+                foreach($dates as $dateJDR)
+                {
+                    $listeDates = $listeDates."--".$dateJDR;
+                }
+                $exec = $jeu->insertJDR($pseudo, $email, $tel, $synopsis, $fournis, $nbrJoueur, $listeDates, 0);
+                if(!$exec)
+                {
+                    $form['valide'] = false;
+                    $form['message'] = "Echec lors de l'enregistrement de la demande";
+                }
+                else
+                    $form['valide'] = true;
+            }
+            else
+            {
+                $form['valide'] = false;
+                $form['message'] = "Vous n'avez pas sélectionné de date !";
+            }
+        }
+        else if(isset($_GET['id']))
         {
             $id = $_GET['id'];
             $form['leJeu'] = $jeu->selectById($id);
-            $dateUnMois = date('D-Y-m-d', strtotime('+1 month'));
             $n = 0;
             for($i = 0; $i<31; $i++)
             {
-                $date = date('D-Y-m-d', strtotime('+'.$i.' day', strtotime($dateUnMois)));
+                $date = date('D-Y-m-d', strtotime('+1 month, +'.$i.' day'));
                 $jourDate = explode("-", $date);
                 if($jourDate[0]!="Mon" && $jourDate[0]!="Sun")
                 {
-                    $tempo = explode("!-", "!".date('Y-m-d', strtotime('+'.$i.'day', strtotime($dateUnMois))));
-                    $tabDateDispo[$n] = $tempo[1];
+                    $tabDateDispo[$n] = date('Y-m-d', strtotime('+1 month, +'.$i.' day'));
                     $n++;
                 }
             }
         }
         else
         {
-            if(isset($_GET['test']))
-            {
-                if($_POST['inputTest']==0)
-                    echo "existe pas";
-                else
-                    echo "existe";
-            }
-            else
-            {
                 $form['valide'] = false;
-                $form['message'] = "Aucun Jeu rensigné";
-            }
+                $form['message'] = "Aucun Jeu renseigné";
         }
         echo $twig->render('creationJDR.html.twig', array('form'=>$form, 'dateDispo'=>$tabDateDispo));
     }
