108af45cb927a23cf9c206fc7773f423b996a499
tmendes18 <tom.men@hotmail.com>
2019-09-10 11:04:20 +0200

    test tom comit

--- src/config/parametres.php
+++
@@ -1,6 +0,0 @@
-<?php
- $config['serveur']='localhost';
- $config['login'] = 'tmendes';
- $config['mdp'] ='wqlrancp';
- $config['bd'] = 'bdsi62019tmendes';
-?>
--- src/modele/_classes.php
+++
@@ -1,7 +0,0 @@
-<?php
- require_once 'class_utilisateur.php';
-  require_once 'class_type.php';
-  require_once 'class_produit.php';
-  require_once 'class_role.php';
-  require_once 'class_catalogue.php';
-?>
